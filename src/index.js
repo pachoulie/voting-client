7211913ed8da285073eb78c9f2433e62247a7ed0
Angelina <souy.angelina@gmail.com>
2016-09-07 22:33:16 +0200

    chore() : add build files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+require('./App.css');
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+console.log('Hello');
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
+//
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
